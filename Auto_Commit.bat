64346fb09edd43bf64f80bb2c67b019381a710d6
Marcio Ferreira <marcioferreira_junior@hotmail.com>
2022-07-28 12:00:59 -0300

    fixed untracked files

--- Auto_Commit.bat
+++ Auto_Commit.bat
@@ -1,10 +1,13 @@
 REM :: Apenas adiciona arquivos rastreados
 
 
-git rm --cached .gitignore.txt
-git untracked
+git add [uncommitted changes you want to keep] && git commit
+git rm -r --cached .
+git add .
 git commit -m "fixed untracked files"
 
+git add -f [files you want to track again]
+git commit -m "Refresh removing files from .gitignore file."
 
 git add -u
 
