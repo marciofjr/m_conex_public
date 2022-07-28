e465f64f3d35631312c37b10a70e3d479ffc6006
Marcio Ferreira <marcioferreira_junior@hotmail.com>
2022-07-28 11:36:53 -0300

    new file:   .gitignore.txt

---
+++ Auto_Commit.bat
@@ -0,0 +1,11 @@
+REM :: Apenas adiciona arquivos rastreados
+git add -u
+
+REM Confirme todos os seus arquivos adicionados
+git commit -m marciao
+
+REM :: Branch (ou seja, master)
+git push origin master 
+
+pause
+exit
